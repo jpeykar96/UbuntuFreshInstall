f89bf3d8ef3a3a8c7c5ffcc11ea3417809dcb174
Jordan Peykar <jpeykar96@gmail.com>
2017-10-12 14:45:47 -0400

    Update setupUbuntu.sh

--- setupUbuntu.sh
+++ setupUbuntu.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y curl git vim default-jre default-jdk synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras bleachbit flashplugin-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 ubuntu-restricted-extras docker emacs gcc
+sudo apt-get install -y curl git vim default-jre default-jdk synaptic unrar zip unzip docker emacs gcc python-pip
